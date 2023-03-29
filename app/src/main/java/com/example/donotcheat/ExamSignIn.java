0d740a65ee8bdc3092f269eb11bdeeecc89b9bd3
fprtm <fprtmjinho>
2023-03-29 19:26:21 +0900

    수정중

--- app/src/main/java/com/example/donotcheat/ExamSignIn.java
+++ app/src/main/java/com/example/donotcheat/ExamSignIn.java
@@ -24,6 +24,7 @@ import com.google.firebase.firestore.QuerySnapshot;
 public class ExamSignIn extends AppCompatActivity {
     private FirebaseAuth auth;
     private FirebaseFirestore db;
+    EditText nameText;
     EditText codeText;
     Button joinButton;
 
@@ -31,19 +32,20 @@ public class ExamSignIn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exam_sign_in);
+        nameText = (EditText) findViewById(R.id.examName);
         codeText = (EditText) findViewById(R.id.examCode);
         joinButton = (Button) findViewById(R.id.join);
         joinButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                joinExam(codeText.getText().toString());
+                joinExam(codeText.getText().toString(),nameText.getText().toString());
             }
         });
     }
-    public void nameInputDlg(String code) {
+    public void nameInputDlg(String code, String name) {
         final LinearLayout linear = (LinearLayout) View.inflate(ExamSignIn.this, R.layout.profile_input_dialog, null);
         AlertDialog.Builder dlg = new AlertDialog.Builder(ExamSignIn.this);
-        dlg.setTitle("방 입장"); //제목
+        dlg.setTitle("시험장 입장"); //제목
 
         if(linear.getParent() != null) ((ViewGroup) linear.getParent()).removeView(linear); // 다이얼로그 여러번 생성시 중복된 뷰그룹 들어가 발생하는 에러처리 부분
         dlg.setView(linear);
@@ -51,6 +53,7 @@ public class ExamSignIn extends AppCompatActivity {
         dlg.setPositiveButton("입장",new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int which) {
                 EditText num = (EditText) linear.findViewById(R.id.usrCode);
+                EditText room = (EditText) linear.findViewById(R.id.examRoomName);
                 EditText name = (EditText) linear.findViewById(R.id.usrName);
                 EditText phone = (EditText) linear.findViewById(R.id.usrPhone);
                 //nameInputDlg를 SignIn 앞에 뷰 하나를 만들어서 거기서 처리하는게 나아보임
@@ -68,7 +71,7 @@ public class ExamSignIn extends AppCompatActivity {
         });
         dlg.show();
     }
-    void joinExam(String code) {
+    void joinExam(String code, String name) {
         final int[] flag = {0};
         if (code == "") {
             Toast.makeText(ExamSignIn.this, "코드를 입력해주세요.", Toast.LENGTH_LONG).show(); return;}
@@ -79,11 +82,13 @@ public class ExamSignIn extends AppCompatActivity {
                     @Override
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
                         String codes;
+                        String names;
                         if (task.isSuccessful()){
                             for (QueryDocumentSnapshot document : task.getResult()){
                                 codes = (String) document.getData().get("code");
-                                if (code.equals(codes) == true){
-                                    nameInputDlg(code);
+                                names = (String) document.getData().get("name");
+                                if (code.equals(codes) == true && name.equals(names) == true){
+                                    nameInputDlg(code,name);
                                     flag[0] = 1;
                                 }
                             }
--- app/src/main/java/com/example/donotcheat/ManagerSignIn.java
+++ app/src/main/java/com/example/donotcheat/ManagerSignIn.java
@@ -1,12 +1,18 @@
 package com.example.donotcheat;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -38,7 +44,7 @@ public class ManagerSignIn extends AppCompatActivity {
         if (code == "") {
             Toast.makeText(ManagerSignIn.this, "코드를 입력해주세요.", Toast.LENGTH_LONG).show(); return;}
         if (code.length() < 4) {Toast.makeText(ManagerSignIn.this, "4자리 코드를 입력해주세요.", Toast.LENGTH_LONG).show(); return;}
-        db.collection("room")
+        db.collection("examRoom")
                 .get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     @Override
@@ -47,7 +53,7 @@ public class ManagerSignIn extends AppCompatActivity {
                         if (task.isSuccessful()){
                             for (QueryDocumentSnapshot document : task.getResult()){
                                 codes = (String) document.getData().get("code");
-                                if (code.equals(codes) == true){
+                                if (code.equals(codes)){
                                     flag[0] = 1;
                                 }
                             }
@@ -56,7 +62,34 @@ public class ManagerSignIn extends AppCompatActivity {
                 });
         if (flag[0]==1){Toast.makeText(ManagerSignIn.this, "중복된 코드 입니다.", Toast.LENGTH_LONG).show(); return;}
         else if(flag[0]==0){
-
+            examCreateDlg(code);
         }
+    }
+    public void examCreateDlg(String code) {
+        final LinearLayout linear = (LinearLayout) View.inflate(ManagerSignIn.this, R.layout.create_exam_dialog, null);
+        AlertDialog.Builder dlg = new AlertDialog.Builder(ManagerSignIn.this);
+        dlg.setTitle("시험장 생성"); //제목
+
+        if(linear.getParent() != null) ((ViewGroup) linear.getParent()).removeView(linear); // 다이얼로그 여러번 생성시 중복된 뷰그룹 들어가 발생하는 에러처리 부분
+        dlg.setView(linear);
+
+        dlg.setPositiveButton("입장",new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                TextView examCode = (EditText) linear.findViewById(R.id.managerCode);
+                EditText name = (EditText) linear.findViewById(R.id.examName);
+                EditText type = (EditText) linear.findViewById(R.id.examType);
+
+                Intent intent = new Intent(getApplicationContext(), Exam.class); // 관리자 뷰로 넘어가야함
+                finish();
+                startActivity(intent);
+            }
+        });
+        dlg.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+        dlg.show();
     }
 }
